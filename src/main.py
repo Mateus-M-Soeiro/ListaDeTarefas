b99185850c05ddd288f353a4776841ba35441470
Mateus-M-Soeiro <mateusmsoeiro@outlook.com.br>
2025-04-03 11:58:06 -0300

    criação dos metadados

--- src/main.py
+++ src/main.py
@@ -2,15 +2,27 @@ import flet as ft
 
 
 
-todo = {}
+to_do = {}
+
 
 
 def main(page: ft.Page):
-    page.title = "Visualização"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD, on_press = )
+
+    def todo(to_do):
+        pass
+
+    page.title = "Visualização"
+    
+    page.appbar = ft.AppBar(
+        
+
+    )
+    page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD)
+    page.update()
+
     
     
 
