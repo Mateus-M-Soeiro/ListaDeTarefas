283f64e941b27b154a444420bc124e8c319be7da
Mateus-M-Soeiro <unisanta@santacecilia.br>
2025-04-11 10:30:59 -0300

    adição do appbar

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from turtle import bgcolor, onkeypress
 import flet as ft
 
 
@@ -6,7 +7,26 @@ to_do = {}
 
 
 
+def Adicionar_Tarefa():
+
+    pass
+
+
+
+
+
+
+
 def main(page: ft.Page):
+
+    def Create_Task():
+        page.route = "/Criar_Task"
+        #pass
+    
+    def route_change():
+
+    
+    page.adaptive = True
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
@@ -18,9 +38,15 @@ def main(page: ft.Page):
     
     page.appbar = ft.AppBar(
         
+        leading= ft.IconButton(ft.icons.MENU),
+        leading_width= 75,
+        title=  ft.Text(page.title),color=ft.colors.BACKGROUND,
+        bgcolor= ft.Colors.INVERSE_SURFACE,center_title= True,
+
+
 
     )
-    page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD)
+    page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.ADD, on_click= Create_Task())
     page.update()
 
     
